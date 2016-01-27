4984e20ad081fc429338e4381d7de442dd846d42
Apisarn Sasuk <apisarn.sasuk@allianz.com>
2016-01-27 15:18:49 +0700

    recode share

--- web/index.php
+++ web/index.php
@@ -1,3 +1,8 @@
-<?php
-    echo "Hello Yose";
-?>
+<html>
+<body>
+<a id="repository-link" href="index.php#readme">https://github.com/varutap/php-scrum-Jan2016.git</a>
+<br />
+<a id="readme">YoseTheGame</a>
+</body>
+</html>
+
