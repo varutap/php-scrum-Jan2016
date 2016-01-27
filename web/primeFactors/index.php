b975cff29f3c0a7988f095879fe74116ea4dc0b4
Varut Aphimanchindakul <varut.aphimanchindakul@allianz-assistance.com>
2016-01-27 15:46:45 +0700

    add new file

---
+++ web/primeFactors/index.php
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+$(document).ready(function() {
+                  var vars = [], hash;
+                  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+                  for(var i = 0; i < hashes.length; i++)
+                  {
+                  hash = hashes[i].split('=');
+                  vars.push(hash[0]);
+                  vars[hash[0]] = hash[1];
+                  }
+                  
+                  });
+
+<?php
+    $temp = vars['number'];
+    $cart = array();
+
+    while($temp > 1) {
+        $temp = $temp / 2;
+        $cart[] = 2;
+        
+    }
+
+    $json = array("number"=>vars['number'],"decomposition"=>$cart);
+    header('Content-type: application/json');
+    echo json_encode($json);
