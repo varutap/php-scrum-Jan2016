2c8499a7a563f3088900e10eb2c0ba0c7d1432dc
Varut Aphimanchindakul <varut.aphimanchindakul@allianz-assistance.com>
2016-01-27 16:29:04 +0700

    commit

--- web/primeFactors/index.php
+++ web/primeFactors/index.php
@@ -1,13 +1,17 @@
 <?php
     $temp = $_GET["number"];
-    $cart = array();
+    if(is_numeric($temp)) {
+        $cart = array();
 
-    while($temp > 1) {
-        $temp = $temp / 2;
-        $cart[] = 2;
-        
+        while($temp > 1) {
+            $temp = $temp / 2;
+            $cart[] = 2;
+            
+        }
+
+        $json = array("number"=>$_GET["number"],"decomposition"=>$cart);
+    } else {
+       $json = array("number"=>$_GET["number"],"error"=>"not a number");
     }
-
-    $json = array("number"=>$_GET["number"],"decomposition"=>$cart);
     header('Content-type: application/json');
     echo json_encode($json);
