e43282784bba64a92ca78703eb60a21d7eedd418
Varut Aphimanchindakul <varut.aphimanchindakul@allianz-assistance.com>
2016-01-27 15:15:10 +0700

    fix index

--- web/index.php
+++ web/index.php
@@ -1,7 +1,3 @@
-<html>
-<body>
-<a id="repository-link" href="index.php#readme">https://github.com/varutap/php-scrum-Jan2016.git</a>
-<br />
-<a id="readme">YoseTheGame</a>
-</body>
-</html>
+<?php
+    echo "Hello Yose";
+?>
