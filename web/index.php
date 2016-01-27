4a142d7e523d9429e3fbb669d793fff303b711bd
Varut Aphimanchindakul <varut.aphimanchindakul@allianz-assistance.com>
2016-01-27 15:24:06 +0700

    modify contact again!

--- web/index.php
+++ web/index.php
@@ -3,18 +3,8 @@
 ?>
 
 <li>
-<a href="#contact-me-link">Contact Me</a>
+<a id="#contact-me-link" href="/contact">Contact Me</a>
 </li>
-.
-.
-.
-.
-.
-.
-.
-.
-
-<a id="contact-me-link">contact-me-link</a>
 
 <h3><span class="glyphicon glyphicon-link"></span> Contact Me</h3>
 <p>Please call me at XXX-XXXX</p>
