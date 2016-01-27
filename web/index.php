83084951732c02674a949a3bccb28e10235304bc
Varut Aphimanchindakul <varut.aphimanchindakul@allianz-assistance.com>
2016-01-27 15:18:06 +0700

    add contact

--- web/index.php
+++ web/index.php
@@ -1,3 +1,20 @@
 <?php
     echo "Hello Yose";
 ?>
+
+<li>
+<a href="#contact-me-link">Contact Me</a>
+</li>
+.
+.
+.
+.
+.
+.
+.
+.
+
+<a name="contact-me-link"></a>
+
+<h3><span class="glyphicon glyphicon-link"></span> Contact Me</h3>
+<p>Please call me at XXX-XXXX</p>
