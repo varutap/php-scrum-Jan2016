e876c694fe001acff22240ade7913f8e0bb9f672
Apisarn Sasuk <apisarn.sasuk@allianz.com>
2016-01-28 15:08:08 +0700

    gates

--- web/astroport/index.php
+++ web/astroport/index.php
@@ -1,1 +1,10 @@
 <h1 id="astroport-name">Krroo Astroport</h1>
+<br />
+<div id="gate-1">gate-1</div><br />
+<div id="ship-1">ship-1</div><br />
+<br />
+<div id="gate-2">gate-2</div><br />
+<div id="ship-2">ship-2</div><br />
+<br />
+<div id="gate-3">gate-3</div><br />
+<div id="ship-3">ship-3</div><br />
