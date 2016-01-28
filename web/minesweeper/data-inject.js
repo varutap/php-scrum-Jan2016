67f6e74f83def2a177b31be6ced2322cccfaea20
PlaySafe <toomtam90@gmail.com>
2016-01-28 16:07:40 +0700

    Sweeper fixed

--- web/minesweeper/data-inject.js
+++ web/minesweeper/data-inject.js
@@ -1,7 +1,28 @@
 var arrayWidth = 8;
 var arrayHeight = 8;
+document.grid = [];
+
+var bombWidth = -1;
+var bombHeight = -1;
 
 function load() {
+    document.grid = [
+        ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+        ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+        ['empty', 'empty', 'empty', 'empty', 'empty', 'bomb', 'empty', 'empty'],
+        ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+        ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+        ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+        ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+        ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+    ];
+};
+
+function clickCell(element) {
+    element.addClass("lost");
+};
+
+function createGrid() {
     var randomWidth = Math.floor((Math.random() * arrayWidth) + 1);
     var randomHeight = Math.floor((Math.random() * arrayHeight) + 1);
     console.log(randomHeight);
@@ -12,18 +33,26 @@ function load() {
         $("#minesweeper").append("<tr id='" + rowId + "'></td>");
         for (var p = 1; p <= arrayHeight; p++) {
             var id = "cell-" + n + "x" + p;
-            if (randomWidth == n && randomHeight == p) {
-                var childAppend = "<td id='" + id + "'><a href='#'>bomb</a></td>";
+            if (randomWidth == bombWidth && randomHeight == bombHeight) {
                 var element = $("#" + rowId);
+                var childAppend = "<td id='" + id + "' onclick='clickCell(this);'>empty</td>";
                 element.append(childAppend);
-                var childElement = $("#" + id);
-                element.click(function () {
-                    childElement.addClass("lost");
-                });
             }
             else {
-                var childAppend = "<td id='" + id + "'>empty</td>";
+                var childAppend = "<td id='" + id + ">empty</td>";
                 $("#" + rowId).append(childAppend);
+            }
+        }
+    }
+};
+
+function setBombPosition() {
+    for (var n = 1; n <= arrayWidth; n++) {
+        for (var p = 1; p <= arrayHeight; p++) {
+            if (document.grid[n - 1][p - 1] === "bomb") {
+                bombWidth = n;
+                bombHeight = p;
+                break;
             }
         }
     }
@@ -31,4 +60,6 @@ function load() {
 
 $(document).ready(function () {
     load();
+    setBombPosition();
+    createGrid();
 });
