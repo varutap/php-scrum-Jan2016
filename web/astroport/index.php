e855d5f59116afdda5d6dec49d4fbc17f0b0c1c4
Apisarn Sasuk <apisarn.sasuk@allianz.com>
2016-01-28 15:31:52 +0700

    docker

--- web/astroport/index.php
+++ web/astroport/index.php
@@ -1,13 +1,28 @@
+<script src="jquery.js"></script>
 <h1 id="astroport-name">Krroo Astroport</h1>
 <br />
 <div id="gate-1">
-<div id="ship-1">ship-1</div><br />
+<div>Ship</div><br />
+<div id="ship-1">-- None --</div><br />
 </div>
 <br />
 <div id="gate-2">
-<div id="ship-2">ship-2</div><br />
+<div>Ship</div><br />
+<div id="ship-2">-- None --</div><br />
 </div>
 <br />
 <div id="gate-3">
-<div id="ship-3">ship-3</div><br />
+<div>Ship</div><br />
+<div id="ship-3">-- None --</div><br />
 </div>
+<br />
+<div>
+  <div>Ship</div><input id="ship" type="text" /><input id="dock" type="button" value="dock" />
+</div>
+
+<script>
+$("#dock").click(function() {
+   $("#ship-1").html($("#ship").val() );
+});
+
+</script>
