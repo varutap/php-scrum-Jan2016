7c18dc55c0478f8124a612b99e674795e271c24c
Varut Aphimanchindakul <varut.aphimanchindakul@allianz-assistance.com>
2016-01-29 14:26:44 +0700

    add button GO

--- web/primeFactors/ui/index.php
+++ web/primeFactors/ui/index.php
@@ -1,4 +1,4 @@
 <h1 id="title">Hello</h1>
 <h2 id="invitation">Sup</h2>
 Number: <input type="text" id="number" name="number">
-<input type="submit" value="Go" id="go">
+<button type="button" id="go">Go</button>
