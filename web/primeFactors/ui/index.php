3bb6880a4ba0ad1a49169b1c756d606aadded176
Varut Aphimanchindakul <varut.aphimanchindakul@allianz-assistance.com>
2016-01-29 14:33:45 +0700

    Test

--- web/primeFactors/ui/index.php
+++ web/primeFactors/ui/index.php
@@ -2,3 +2,8 @@
 <h2 id="invitation">Sup</h2>
 Number: <input type="text" id="number" name="number">
 <button type="button" id="go">Go</button>
+<?php
+    echo "hello2";
+$json = file_get_contents('http://sheltered-peak-44185.herokuapp.com/primeFactors/?number=300');
+$obj = json_decode($json);
+echo $obj->access_token;
