cc8ca22d1ed0bf2ff50f28f6b60e7345625bd210
Varut Aphimanchindakul <varut.aphimanchindakul@allianz-assistance.com>
2016-01-29 14:10:53 +0700

    prime factor

--- web/primeFactors/index.php
+++ web/primeFactors/index.php
@@ -9,29 +9,35 @@
         }
         return array($num);
     }
-    
-    $temp = $_GET["number"];
+    $xx = $_SERVER['QUERY_STRING'];
+    $qs = preg_replace("/(?<=^|&)(\w+)(?==)/", "$1[]", $_SERVER["QUERY_STRING"]);
+    parse_str($qs, $new_GET);
+    $jsons = array();
+    for($j = 0; $j < count($new_GET['number']); $j++) {
+        $temp = $new_GET['number'][$j];
+        if(is_numeric($temp)) {
+    //        $cart = array();
+    //
+    //        while($temp > 1) {
+    //            $temp = $temp / 2;
+    //            $cart[] = 2;
+    //            
+    //        }
+            
+            if($temp > 1000000) {
+                $json = array("number"=>$temp,"error"=>"too big number (>1e6)");
+            } else {
+            
+                $cart = primefactor($temp);
+                $cart = array_reverse($cart);
 
-    if(is_numeric($temp)) {
-//        $cart = array();
-//
-//        while($temp > 1) {
-//            $temp = $temp / 2;
-//            $cart[] = 2;
-//            
-//        }
-        
-        if($temp > 1000000) {
-            $json = array("number"=>$_GET["number"],"error"=>"too big number (>1e6)");
+                $json = array("number"=>$temp,"decomposition"=>$cart);
+            }
         } else {
-        
-            $cart = primefactor($temp);
-            $cart = array_reverse($cart);
-
-            $json = array("number"=>$_GET["number"],"decomposition"=>$cart);
+           $json = array("number"=>$temp,"error"=>"not a number");
         }
-    } else {
-       $json = array("number"=>$_GET["number"],"error"=>"not a number");
+        $jsons[] = $json;
     }
-    header('Content-type: application/json');
-    echo json_encode($json);
+
+    header('Content-Type: application/json');
+    echo json_encode($jsons);
